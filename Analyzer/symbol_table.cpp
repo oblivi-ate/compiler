ae19c8d1d6d7f7b1451ad902ad437b0a0bbca145
108553577 <158500056+108553577@users.noreply.github.com>
2025-01-15 13:50:32 +0800

    Update symbol_table.cpp

--- Analyzer/symbol_table.cpp
+++ Analyzer/symbol_table.cpp
@@ -1,119 +1,99 @@
-#include <stdexcept>
+// symbol_table.cpp
+
+#include "symbol_table.h"
 #include <iostream>
 #include <iomanip>
-#include "symbol_table.h"
+#include <cstdlib>
+#include <cstring>
 
-// 符号类型名称数组，用于打印符号类型
-static std::string exprTypeNames[] = {
-    "Void", "Int"
-};
+//define posttable
+PycNode::PycNode(char type, int pos)
+    : nodeType(type), nextPostable(nullptr), next(nullptr)
+{
+    nextPostable = new Postable{nullptr, pos};
+}
 
-// PycSymRef 构造函数实现
-PycSymRef::PycSymRef(PycSymbol* p, TreeNode* rN)
-    : parent(p), refNode(rN)
+// define symbol table
+Symbol_table::Symbol_table(char somethingChar)
+    : table(nullptr), something(somethingChar), next(nullptr)
 {
 }
 
-// PycSymbol 构造函数实现
-PycSymbol::PycSymbol(SymbolTable* p, TreeNode* dN, const std::string& type)
-    : parent(p), declNode(dN), typeName(type)
-{
+//hash function
+unsigned long hash_function(char nodeType) {
+    unsigned long hash = 5381;
+    hash = ((hash << 5) + hash) + nodeType; // hash * 33 + nodeType
+    return hash;
 }
 
-// 初始化静态成员变量
-int SymbolTable::internal_id = 0;
-
-// SymbolTable 构造函数实现（自动分配 ID）
-SymbolTable::SymbolTable(TreeNode* t)
-    : tableNode(t), id(internal_id++), lower(nullptr), next(nullptr), nextAttachPoint(&lower)
-{
+// get hash value
+unsigned long get_symbol_hash(char nodeType) {
+    return hash_function(nodeType);
 }
 
-// SymbolTable 构造函数实现（指定 ID）
-SymbolTable::SymbolTable(TreeNode* t, int id)
-    : tableNode(t), id(id), lower(nullptr), next(nullptr), nextAttachPoint(&lower)
-{
-    SymbolTable::internal_id = id + 1;
-}
-
-// 查找符号实现
-PycSymbol& SymbolTable::lookup(const std::string& name)
-{
-    auto it = hashTable.find(name);
-    if (it == hashTable.end())
-    {
-        if (auto upperShared = upper.lock())
-        {
-            return upperShared->lookup(name);
+// print table
+void traverse_symbol_table(const Symbol_table* table) {
+    while (table != nullptr) {
+        std::cout << "Symbol Table: " << table 
+                  << ", Something: " << table->something << std::endl;
+        const PycNode* node = table->table;
+        while (node != nullptr) {
+            std::cout << "\tPycNode: nodeType: " << node->nodeType << std::endl;
+            const Postable* post = node->nextPostable;
+            while (post != nullptr) {
+                std::cout << "\t\tPostable: table: " << post->table 
+                          << ", position: " << post->position << std::endl;
+                post = post->table;
+            }
+            node = node->next;
         }
-        else
-        {
-            throw std::invalid_argument("Symbol \"" + name + "\" not found!");
+        table = table->next;
+    }
+}
+
+// add new node
+void add_symbol(Symbol_table* table, char nodeType, int position) {
+    if (table == nullptr) {
+        std::cerr << "Error: Symbol table is NULL." << std::endl;
+        return;
+    }
+
+ 
+    PycNode* newNode = new PycNode(nodeType, position);
+    if (!newNode) {
+        std::cerr << "Error: Unable to allocate memory for PycNode." << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+
+    newNode->next = table->table;
+    table->table = newNode;
+}
+//create new table
+Symbol_table* create_symbol_table(char somethingChar) {
+    Symbol_table* table = new Symbol_table(somethingChar);
+    if (!table) {
+        std::cerr << "Error: Unable to allocate memory for Symbol_table." << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+    return table;
+}
+//destroy table
+void destroy_symbol_table(Symbol_table* table) {
+    while (table != nullptr) {
+        PycNode* node = table->table;
+        while (node != nullptr) {
+            Postable* post = node->nextPostable;
+            while (post != nullptr) {
+                Postable* tempPost = post;
+                post = post->table;
+                delete tempPost;
+            }
+            PycNode* tempNode = node;
+            node = node->next;
+            delete tempNode;
         }
+        Symbol_table* tempTable = table;
+        table = table->next;
+        delete tempTable;
     }
-    return it->second;
 }
-
-// 检查当前符号表是否包含指定符号
-bool SymbolTable::check_local(const std::string& name)
-{
-    return hashTable.find(name) != hashTable.end();
-}
-
-// 打印符号表内容实现
-void SymbolTable::print_table()
-{
-    static int flag = 0;
-    if (!flag)
-    {
-        std::cout << std::left
-                  << std::setw(6) << "Table"
-                  << std::setw(20) << "Name"
-                  << std::setw(20) << "Type"
-                  << std::setw(5) << "Decl"
-                  << "Refs" << std::endl;
-        std::cout << std::left
-                  << std::setw(6) << "ID"
-                  << std::setw(20) << ""
-                  << std::setw(20) << ""
-                  << std::setw(5) << "Line"
-                  << "Lines" << std::endl;
-        std::cout << std::left
-                  << std::setw(6) << "----"
-                  << std::setw(20) << "----"
-                  << std::setw(20) << "----"
-                  << std::setw(5) << "----"
-                  << "----" << std::endl;
-    }
-
-    std::string idField = std::to_string(id);
-    for (const auto& [name, sym] : hashTable)
-    {
-        std::cout << std::left
-                  << std::setw(6) << idField
-                  << std::setw(20) << name
-                  << std::setw(20) << sym.typeName;
-
-        TreeNode* tr = sym.declNode;
-        std::cout << std::setw(5) << tr->line << " ";
-
-        // 打印引用行号
-        for (const auto& ref : sym.refs)
-        {
-            std::cout << ref.refNode->line << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    flag++;
-    if (lower)
-    {
-        lower->print_table();
-    }
-    if (next)
-    {
-        next->print_table();
-    }
-    flag--;
-}
-
